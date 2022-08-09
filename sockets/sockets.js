7a9b9d7547bc8dece761e0e25605574c61c1445a
UrielBm <urielbenitez96@hotmail.com>
2022-08-09 16:30:33 -0500

    create room's with sockets with sessionGame

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -5,16 +5,26 @@ class Sockets {
   }
   socketsEvents() {
     this.io.on("connection", (socket) => {
-      console.log("nuevo cliente conectado");
-      socket.on("action-ship", (action) => {
-        console.log(action);
-        this.io.emit("move-ship", {
-          code: action.code,
-          prev: action.prev,
-          go: action.go,
-        });
+      console.log(`sesion de socket iniciada id del socket: ${socket.id}`);
+      socket.on("session", ({ sessionGame }) => {
+        console.log(`id del juego y del control}: ${sessionGame}`);
+        // creando una sala de socket
+        socket.join(sessionGame);
+      });
+      socket.on("action-ship", (data) => {
+        //escuchando las instrucciones del control
+        console.log(data);
+        // emitiendo un evento al id de la session del juego y del control
+        this.io.to(data.sessionGame).emit("move-ship", data);
       });
     });
   }
 }
 module.exports = Sockets;
+
+// socket.on("action-ship", (action) => {
+//   console.log(action);
+//   this.io.to(idgame).emit("move-ship", (data) => {
+//     console.log(data);
+//   });
+// });
